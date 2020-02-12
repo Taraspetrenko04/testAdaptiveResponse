81d330a7677ed9cef252d882fe155068d9ab742c
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2020-02-12 17:25:31 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-    console.log('Test')
+
+    let menuBtn = document.querySelector('.header__button-menu');
+    let container = document.querySelector('.container');
+    let menuNav = document.querySelector('.header__nav');
+
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        container.classList.toggle('active');
+        menuNav.classList.toggle('active');   
+    })
 }
